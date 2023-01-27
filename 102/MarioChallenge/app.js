9c949d5286b767e8c0e2a90d3d8431984648f955
Yuya Hirano <v322sk@gmail.com>
2023-01-27 13:14:25 -0800

    done

--- 102/MarioChallenge/app.js
+++ 102/MarioChallenge/app.js
@@ -1,45 +1,62 @@
 function isTouching(a, b) {
-	const aRect = a.getBoundingClientRect();
-	const bRect = b.getBoundingClientRect();
-
-	return !(
-		aRect.top + aRect.height < bRect.top ||
-		aRect.top > bRect.top + bRect.height ||
-		aRect.left + aRect.width < bRect.left ||
-		aRect.left > bRect.left + bRect.width
-	);
+  const aRect = a.getBoundingClientRect();
+  const bRect = b.getBoundingClientRect();
+  return !(
+    aRect.top + aRect.height < bRect.top ||
+    aRect.top > bRect.top + bRect.height ||
+    aRect.left + aRect.width < bRect.left ||
+    aRect.left > bRect.left + bRect.width
+  );
 }
 
 const init = () => {
-    //get the avatar
-    // const avatar = document.querySelector(".avatar")
-    //get the coin
+  //get the avatar
+  const avatar = document.querySelector("#avatar");
+  //get the coin
+  const coin = document.querySelector("#coin");
 
-    moveCoin();
-    window.addEventListener('keyup', function(e){
-        if(e.key === 'ArrowDown' || e.key === 'Down'){
-            moveVertical(avatar, 50);
-        }
+  moveCoin();
+  window.addEventListener("keyup", function (e) {
+    if (e.key === "ArrowDown" || e.key === "Down") {
+      moveVertical(avatar, 50);
+    } else if (e.key === "ArrowUp" || e.key === "Up") {
+      moveVertical(avatar, -50);
+    } else if (e.key === "ArrowLeft" || e.key === "Left") {
+      moveHorizontal(avatar, -50);
+      avatar.style.transform = "scale(-1, 1)";
+    } else if (e.key === "ArrowRight" || e.key === "Right") {
+      moveHorizontal(avatar, 50);
+      avatar.style.transform = "scale(1, 1)";
+    } else return;
 
-        if(isTouching(avatar,coin)) moveCoin();
-    });
-}
+    if (isTouching(avatar, coin)) moveCoin();
+  });
+};
 
 const moveVertical = (element, amount) => {
-    const currTop = extractPos(element.style.top);
+  const currTop = extractPos(element.style.top);
+  if (currTop + amount < window.innerHeight - 200 && currTop + amount > 0) {
     element.style.top = `${currTop + amount}px`;
-}
+  }
+};
+
+const moveHorizontal = (element, amount) => {
+  const currLeft = extractPos(element.style.left);
+  if (currLeft + amount < window.innerWidth - 100 && currLeft + amount > 0) {
+    element.style.left = `${currLeft + amount}px`;
+  }
+};
 
 const extractPos = (position) => {
-    if(!position) return 100;
-    return parseInt(position.slice(0, -2))
-}
+  if (!position) return 100;
+  return parseInt(position.slice(0, -2));
+};
 
 const moveCoin = () => {
-    const x = Math.floor(Math.random() * window.innerWidth)
-    // const y = ?
-    coin.style.top = `${x}px`;
-    // coin.style.?? = ??
-}
+  const y = Math.floor(Math.random() * (window.innerHeight - 200));
+  const x = Math.floor(Math.random() * (window.innerWidth - 200));
+  coin.style.top = `${y}px`;
+  coin.style.left = `${x}px`;
+};
 
 init();
